ac2f3aef802f22c2827068bc6bd11fb19fc54e07
Shanith K K <shanith.kk@hugobyte.com>
2023-12-27 13:19:15 +0530

    chore: test

--- Formula/composer-test.rb
+++ Formula/composer-test.rb
@@ -1,6 +1,6 @@
 class Espanso < Formula
     desc "Composer Language"
-    homepage "https://github.com/shanithkk/aurras"
+    homepage "https://github.com/shanithkk/composer-test"
     url "https://github.com/shanithkk/aurras/releases/download/0.0.1/composer-mac.tar.gz"
     sha256 "af8b599612c2f404ded794c1dfdf34f60a78ee327c28aaf98f30efbf20fed414"
     version "0.0.1"
